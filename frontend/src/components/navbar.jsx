d1357f5699c0fcd1953eddd1be95d063e188be09
Kayko Ohkawa <ohkawa2.0@gmail.com>
2025-09-13 10:54:58 -0400

    navbar tweaks and code cleanup

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -131,6 +131,7 @@ export default function NavBar({ activeSection, scrollToSection }) {
 
         {/* right items */}
         <div className="flex items-center text-lg space-x-2">
+
           {/* box icons */}
           <div className={`flex items-center p-2 rounded-sm transition-colors duration-500 border-2 
             ${scrolled 
@@ -138,22 +139,22 @@ export default function NavBar({ activeSection, scrollToSection }) {
               : "border-offwhite/15 dark:border-seagreen-200/15" 
             }`}
           >
-            <a className="px-2 md:px-3 py-1 navbar-icon fill-offwhite" href="https://github.com/halfriver" title='GitHub' target="_blank" rel="noreferrer">
+            <a className="px-2 sm:px-3 py-1 navbar-icon fill-offwhite" href="https://github.com/halfriver" title='GitHub' target="_blank" rel="noreferrer">
               <Github width={25} height={25} strokeWidth={2.5} />
             </a>
-            <a className="px-2 md:px-3 py-1 navbar-icon" href="https://www.linkedin.com/in/ohkawak/" title='LinkedIn' target="_blank" rel="noreferrer">
+            <a className="px-2 sm:px-3 py-1 navbar-icon" href="https://www.linkedin.com/in/ohkawak/" title='LinkedIn' target="_blank" rel="noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
               </svg>
             </a>
-            <a className="px-2 md:px-3 py-1 navbar-icon" href="mailto:you@example.com">
+            <a className="px-2 sm:px-3 py-1 navbar-icon" href="mailto:you@example.com">
               <Mail width={25} height={25} strokeWidth={2} />
             </a>
           </div>
 
           {/* language dropdown */}
           <details className="dropdown mr-2" title='Language Select'>
-            <summary className="block navbar-icon cursor-pointer px-2 py-1">
+            <summary className="block navbar-icon cursor-pointer px-2 sm:px-4 py-1">
               <Languages />
             </summary>
             <ul
@@ -221,34 +222,6 @@ export default function NavBar({ activeSection, scrollToSection }) {
           </label>
         </div>
       </div>
-      {/* Mobile slide-down links (shown when hamburger open) */}
-      {/* <div
-        id="mobile-nav"
-        className={`lg:hidden px-2 ml-25 rounded-sm overflow-hidden transition-[max-height] duration-300 shadow bg-seagreen-200 dark:bg-seablue-700 w-50 ${
-          isOpen ? "max-h-64" : "max-h-0"
-        }`}
-      >
-        <ul className="px-1 pb-3">
-          {navItems.map(({ id, default: text }) => (
-            <li key={id}>
-              <button
-                onClick={() => {
-                  scrollToSection(id);
-                  setIsOpen(false);
-                }}
-                aria-current={activeSection === id ? "page" : undefined}
-                className={`block w-full text-left py-2 text-base hover:bg-seagreen-100 dark:hover:bg-seablue-600 ${
-                  activeSection === id
-                    ? "text-offwhite dark:text-seagreen-200 font-medium"
-                    : "text-offwhite/90 dark:text-seagreen-200/80"
-                }`}
-              >
-                {text}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div> */}
     </nav>
   );
 }
